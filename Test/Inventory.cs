494dddc9c2032f68b2e2b0b75d93a35de839acad
Andrei Zhurko <andrey675246zhurko@mail.ru>
2025-12-25 23:51:52 +0300

    Add project files.

---
+++ Test/Inventory.cs
@@ -0,0 +1,88 @@
+﻿namespace Test;
+
+internal class Inventory
+{
+    private readonly List<Item> _items = [];
+    private const int MaxWeight = 100;
+    private readonly Lock _lock = new ();   
+
+    public IReadOnlyCollection<Item> Items 
+    {
+        get 
+        {
+            lock (_lock) 
+            {
+                return _items.ToList().AsReadOnly();
+            }
+            
+        }
+    }
+
+    public int TotalWeight
+    {
+        get 
+        {
+            lock (_lock) 
+            {
+                return _items.Sum(x => x.Weight);
+            }
+            
+        }
+    }
+    public void AddItem(Item item)
+    {
+        ArgumentNullException.ThrowIfNull(item);
+
+        lock (_lock) 
+        {
+            var newTotalWight = TotalWeight + item.Weight;
+
+            if (newTotalWight > MaxWeight)
+            {
+                throw new InvalidOperationException("Inventory weight limit exceeded.");
+            }
+
+            var existingElement = _items.FirstOrDefault(x => x.Name.Equals(item.Name, StringComparison.OrdinalIgnoreCase));
+
+            if (existingElement != null)
+            {
+                _items.Remove(existingElement);
+                _items.Add(new Item(existingElement.Name, existingElement.Weight + item.Weight));
+                return;
+            }
+
+            _items.Add(item);
+        }
+        
+    }
+
+    public bool RemoveItem(Item item)
+    {
+        var removeItem = _items.FirstOrDefault(x => x.Name == item.Name);
+        if (removeItem == null) 
+        {
+            return false;
+        }
+
+        lock (_lock) 
+        {
+            return _items.Remove(removeItem);
+        }
+        
+    }
+
+    public IReadOnlyList<Item> FindByName(string substring)
+    {
+        if (string.IsNullOrEmpty(substring))
+        {
+            return Array.Empty<Item>();
+        }
+
+        lock (_lock) 
+        {
+            return _items.Where(x => x.Name.Contains(substring, StringComparison.OrdinalIgnoreCase))
+                     .ToList()
+                     .AsReadOnly();
+        }       
+    }
+}
---
+++ UnitTesting/InventoryTest.cs
@@ -0,0 +1,114 @@
+﻿using FluentAssertions;
+using Test;
+
+namespace UnitTesting
+{
+    public class InventoryTest
+    {
+        [Fact]
+        public void AddItem_WhenItemIsValid_AddsItemToInventory() 
+        {
+            //Arrange
+            var inventory = new Inventory();
+            var item = new Item("Sword", 10);
+
+            //Act
+            inventory.AddItem(new Item("Sword", 10));
+
+            //Assert
+            inventory.Items.Should().HaveCount(1);
+
+            var addedItem = inventory.Items.FirstOrDefault();
+
+            addedItem.Name.Should().Be("Sword");
+            addedItem.Weight.Should().Be(10);
+        }
+
+        [Fact]
+        public void AddItem_WhenItemWithSameNameExists_AddsItemToInventory() 
+        {
+            //Arrange
+            var inventory = new Inventory();
+            var item1 = new Item("Sword", 10);
+            var item2 = new Item("Sword", 10);
+
+            //Act
+            inventory.AddItem(item1);
+            inventory.AddItem(item2);
+
+            //Assert
+            inventory.Items.Should().HaveCount(1);
+            var addedItem = inventory.Items.FirstOrDefault();
+            addedItem.Weight.Should().Be(20);
+        }
+
+        [Fact]
+        public void AddItem_WhenAddingItemExceedsMaxWeight_ThrowsInvalidOperationException() 
+        {
+            //Arrange
+            var invertory = new Inventory();
+            var item1 = new Item("Sword", 50);
+            var item2 = new Item("Sword1", 60);
+
+            //Act
+            invertory.AddItem(item1);
+            Action act = () => invertory.AddItem(item2);
+
+            //Assert
+            act.Should().Throw<InvalidOperationException>();
+
+        }
+
+        [Fact]
+        public void RemoveItem_WhenItemExistsInInventory_RemovesItem()
+        {
+            //Arrange
+            var inventory = new Inventory();
+            var item1 = new Item("Sword", 50);
+            inventory.AddItem(item1);
+
+            //Act
+          var result = inventory.RemoveItem(item1);
+
+            //Assert
+            inventory.Items.Should().BeEmpty();
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void FindByName_WhenItemExistsInInventory_FindsItem() 
+        {
+            //Arrange
+            var inventory = new Inventory();
+            var item1 = new Item("Sword", 50);
+            inventory.AddItem(item1);
+
+            //Act
+           var result = inventory.FindByName(item1.Name);
+
+            //Assert           
+            result.Should().ContainSingle(result => result.Name == item1.Name);
+        }
+
+        [Fact]
+        public async Task AddItem_WhenItemsAreAddedConcurrently_AddsAllItems()
+        {
+            //Arrage
+            var inventory = new Inventory();
+            var tasks = Enumerable.Range(0, 10).Select(i => Task.Run(() =>
+            {
+                inventory.AddItem(new Item($"Sword{i}", 10));
+            })).ToArray();
+
+            await Task.WhenAll(tasks);
+
+            //Act
+            var items = inventory.Items;
+            var totalWeight = inventory.TotalWeight;
+
+            //Assert
+            items.Should().HaveCount(10);
+            totalWeight.Should().Be(100);
+        }
+    }
+}
